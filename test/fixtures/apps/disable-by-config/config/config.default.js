d22e7c7c82508d19ab161b93c8f748c809ad581f
fengmk2 <m@fengmk2.com>
2016-07-20 11:20:19 +0800

    test: improve coverage to 100%

--- test/fixtures/apps/disable-by-config/config/config.default.js
+++ test/fixtures/apps/disable-by-config/config/config.default.js
@@ -1,6 +1,7 @@
 'use strict';
 
 exports.alinode = {
+  enable: false,
   appid: process.env.ALINODE_APPID,
   secret: process.env.ALINODE_SECRET,
 };
@@ -8,5 +9,3 @@ exports.alinode = {
 if (process.env.ALINODE_SERVER) {
   exports.alinode.server = process.env.ALINODE_SERVER;
 }
-
-exports.keys = 'foo';

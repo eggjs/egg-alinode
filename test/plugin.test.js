d919e3147f9aab1a31f392dafe157ad9210f6ea1
fengmk2 <m@fengmk2.com>
2016-08-10 17:16:08 +0800

    fix: auto remove 7 days ago log files (#2)
    closes https://github.com/eggjs/egg/issues/23

--- test/plugin.test.js
+++ test/plugin.test.js
@@ -1,14 +1,21 @@
 'use strict';
 
-const request = require('supertest-as-promised');
+const request = require('supertest');
 const mm = require('egg-mock');
 const path = require('path');
+const fs = require('fs');
+const glob = require('glob');
+const moment = require('moment');
+const assert = require('power-assert');
 
 describe('test/plugin.test.js', () => {
   afterEach(mm.restore);
 
   describe('config.alinode.enable = true', () => {
     let app;
+    const schedule = path.join(__dirname, '../app/schedule/removeLogs');
+    const now = moment().startOf('date');
+
     before(() => {
       app = mm.app({
         baseDir: 'apps/demo',
@@ -25,6 +32,71 @@ describe('test/plugin.test.js', () => {
       .get('/')
       .expect('hello alinode, enable: true, env: unittest')
       .expect(200);
+    });
+
+    it('should remove old log files', function* () {
+      fs.writeFileSync(path.join(app.config.alinode.logdir, 'foo-00000000.log'), 'foo');
+      fs.writeFileSync(path.join(app.config.alinode.logdir, 'node-00000000.log'), 'foo');
+      fs.writeFileSync(path.join(app.config.alinode.logdir,
+        `foo-${now.format('YYYYMMDD')}.log`), 'foo');
+      fs.writeFileSync(path.join(app.config.alinode.logdir,
+        `node-${now.format('YYYYMMDD')}.log`), 'foo');
+      fs.writeFileSync(path.join(app.config.alinode.logdir,
+        `access-${now.format('YYYYMMDD')}.log`), 'foo');
+      fs.writeFileSync(path.join(app.config.alinode.logdir,
+        `node-${now.clone().subtract(1, 'days').format('YYYYMMDD')}.log`), 'foo');
+      fs.writeFileSync(path.join(app.config.alinode.logdir,
+        `node-${now.clone().subtract(5, 'days').format('YYYYMMDD')}.log`), 'foo');
+      fs.writeFileSync(path.join(app.config.alinode.logdir,
+        `node-${now.clone().subtract(6, 'days').format('YYYYMMDD')}.log`), 'foo');
+      fs.writeFileSync(path.join(app.config.alinode.logdir,
+        `node-${now.clone().subtract(7, 'days').format('YYYYMMDD')}.log`), 'foo');
+      fs.writeFileSync(path.join(app.config.alinode.logdir,
+        `node-${now.clone().subtract(8, 'days').format('YYYYMMDD')}.log`), 'foo');
+      fs.writeFileSync(path.join(app.config.alinode.logdir,
+        `node-${now.clone().subtract(9, 'days').format('YYYYMMDD')}.log`), 'foo');
+      fs.writeFileSync(path.join(app.config.alinode.logdir,
+        `node-${now.clone().subtract(30, 'days').format('YYYYMMDD')}.log`), 'foo');
+      fs.writeFileSync(path.join(app.config.alinode.logdir,
+        `access-${now.clone().subtract(6, 'months').format('YYYYMMDD')}.log`), 'foo');
+      fs.writeFileSync(path.join(app.config.alinode.logdir,
+        `access-${now.clone().subtract(1, 'years').format('YYYYMMDD')}.log`), 'foo');
+
+      yield app.runSchedule(schedule);
+
+      const files = glob.sync(path.join(app.config.alinode.logdir, '*.log'));
+      assert(files.length === 9);
+
+      assert(fs.existsSync(path.join(app.config.alinode.logdir, 'foo-00000000.log')));
+      assert(fs.existsSync(path.join(app.config.alinode.logdir, 'node-00000000.log')));
+      assert(fs.existsSync(path.join(app.config.alinode.logdir,
+        `foo-${now.format('YYYYMMDD')}.log`)));
+      assert(fs.existsSync(path.join(app.config.alinode.logdir,
+        `node-${now.format('YYYYMMDD')}.log`)));
+      assert(fs.existsSync(path.join(app.config.alinode.logdir,
+        `access-${now.format('YYYYMMDD')}.log`)));
+      assert(fs.existsSync(path.join(app.config.alinode.logdir,
+        `node-${now.clone().subtract(1, 'days').format('YYYYMMDD')}.log`)));
+      assert(fs.existsSync(path.join(app.config.alinode.logdir,
+        `node-${now.clone().subtract(5, 'days').format('YYYYMMDD')}.log`)));
+      assert(fs.existsSync(path.join(app.config.alinode.logdir,
+        `node-${now.clone().subtract(6, 'days').format('YYYYMMDD')}.log`)));
+      assert(fs.existsSync(path.join(app.config.alinode.logdir,
+        `node-${now.clone().subtract(7, 'days').format('YYYYMMDD')}.log`)));
+
+      assert(!fs.existsSync(path.join(app.config.alinode.logdir,
+        `node-${now.clone().subtract(8, 'days').format('YYYYMMDD')}.log`)));
+      assert(!fs.existsSync(path.join(app.config.alinode.logdir,
+        `node-${now.clone().subtract(9, 'days').format('YYYYMMDD')}.log`)));
+      assert(!fs.existsSync(path.join(app.config.alinode.logdir,
+        `node-${now.clone().subtract(30, 'days').format('YYYYMMDD')}.log`)));
+      assert(!fs.existsSync(path.join(app.config.alinode.logdir,
+        `access-${now.clone().subtract(6, 'months').format('YYYYMMDD')}.log`)));
+      assert(!fs.existsSync(path.join(app.config.alinode.logdir,
+        `access-${now.clone().subtract(1, 'years').format('YYYYMMDD')}.log`)));
+
+      // run again should work
+      yield app.runSchedule(schedule);
     });
   });
 
